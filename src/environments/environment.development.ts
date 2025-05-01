12fd9f0d92b77c0c931899bb5c76e071603bdbf1
Alejo-develop <yeyito872@gmail.com>
2025-04-30 21:42:33 -0500

    I added a new apikey to get gifs, also i created a giphy service to http peticions with their first method, 'loadgifs'

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,7 @@ export const environment = {
   companyName: 'Gifs',
   companyName2: 'App',
   companySlogan: 'Manage your gifs',
+
+  gifsApiKey: 'vQiOkYwS3nGAxq5GWzTMm7yInVgE9EUh',
+  giphyApiUrl: 'https://api.giphy.com/v1'
 };
